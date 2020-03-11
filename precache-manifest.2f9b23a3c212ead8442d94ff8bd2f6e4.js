812c8fb4b211aa3162f79f2b344e63d62c517a0a
KivilevaXenia <kivilevaxenia@gmail.com>
2020-03-11 18:20:22 +0300

    corrections

---
+++ precache-manifest.2f9b23a3c212ead8442d94ff8bd2f6e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae935f274ed1f66cf2c5cf3a98be2dcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb70a613cf75e556b500",
+    "url": "/static/css/main.27a30a3f.chunk.css"
+  },
+  {
+    "revision": "7943e3559bce4167a2a8",
+    "url": "/static/js/2.34711356.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.34711356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb70a613cf75e556b500",
+    "url": "/static/js/main.eb65180f.chunk.js"
+  },
+  {
+    "revision": "b5679954dafe0fabf008",
+    "url": "/static/js/runtime-main.ba14cb4c.js"
+  }
+]);
